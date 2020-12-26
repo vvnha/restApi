80bc68a37a501f0be32fcf304d9855b1c4007818
tvquy17it3 <38048486+tvquy17it3@users.noreply.github.com>
2020-12-26 15:19:10 +0700

    Update Permission.php

--- app/Http/Middleware/Permission.php
+++ app/Http/Middleware/Permission.php
@@ -17,7 +17,7 @@ class Permission
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        dd(Auth::guard($guard)->user()->positionID != 1);
+        dd(Auth::guard($guard)->user()->positionID);
         if (Auth::guard($guard)->user()->positionID != 1) {
             return redirect('/');
         }
