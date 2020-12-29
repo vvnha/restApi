f1d63eaf4c5de252db759319fe8e89dcbc04bc4e
vvnha <vvnha.17it3@sict.dn.vn>
2020-12-29 10:06:28 +0700

    update user

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 use App\Http\Controllers\Controller;
 use Carbon\Carbon;
 use App\User;
