b86339f1358fde80dc8af7089b05db5e76b975dc
vvnha <vvnha.17it3@sict.dn.vn>
2021-01-03 15:54:58 +0700

    oke

--- app/Http/Controllers/Api/OrderTbController.php
+++ app/Http/Controllers/Api/OrderTbController.php
@@ -140,7 +140,7 @@ class OrderTbController extends Controller
         if ($datetime->diffInHours($itemOrderDate) <= 2) {
           //   $result = array_push($result, $order);
           //$result = $items->perNum + "," + $result;
-          echo $result = $items->perNum + "," + $result;
+          echo $result = "," + $items->perNum;
         }
       }
       return response()->json(['success' => true, 'code' => '200', 'data' => $result]);
