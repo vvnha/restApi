c07cadefbb9d7ee5de6052014b8f05c84ef5525c
vvnha <vvnha.17it3@sict.dn.vn>
2021-01-03 14:12:13 +0700

    oke

--- app/Http/Controllers/Api/OrderTbController.php
+++ app/Http/Controllers/Api/OrderTbController.php
@@ -135,7 +135,7 @@ class OrderTbController extends Controller
     $order = OrderTb::where('orderDate', 'LIKE', '%' . $dateInput . '%')->get();
 
     if ($order == true) {
-      return response()->json(['success' => true, 'code' => '200', 'data' => $datetime]);
+      return response()->json(['success' => true, 'code' => '200', 'data' => $minute]);
     } else {
       return response()->json(['success' => false, 'code' => '404']);
     }
