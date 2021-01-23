90fea0ccf4e597066e5464179c5dc6b1cfbc4b7c
vvnha <vvnha.17it3@sict.dn.vn>
2021-01-23 12:09:53 +0700

    ok

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -18,10 +18,8 @@ class AdminController extends Controller
       $order = OrderTb::where('orderDate', 'LIKE', '%' . $dateInput . '%')->get();
       // $order = OrderTb::where('orderDate', 'LIKE', '%' . '2021-01-07' . '%')->get();
       
-      dd(count($order) > 0);
 
     	if (count($order) > 0) {
-        
     	   $result = array();
 	       foreach ($order as $items) {
     	        if ($items->service == '0' || $items->service == '1') {
@@ -33,10 +31,7 @@ class AdminController extends Controller
                         $t = explode( '//', $d);
                         $result = array_merge($result, $t);
                     }      	
-    	        }else{
-                    $sb = array(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15);
-                    return view('admin.index',['sb'=>$sb, 'datas'=>"0",'dateS'=> $dateInput]);   
-                }
+    	        }
           }
 	       return view('admin.index',['sb'=>$result,'datas'=>"1",'dateS'=> $dateInput]);
 	    }else {
