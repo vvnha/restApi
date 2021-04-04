ff4ec69a0bd793ca65d1edcf18a6faf704a488a2
vvnha <vvnha.17it3@sict.dn.vn>
2021-04-04 20:30:18 +0700

    delete some update migrate

--- database/migrations/2021_04_04_173746_update_attendance_table.php
+++ database/migrations/2021_04_04_173746_update_attendance_table.php
@@ -14,7 +14,15 @@ class UpdateAttendanceTable extends Migration
     public function up()
     {
         Schema::table('attendances', function (Blueprint $table) {
+            $table->string('note')->after('userID')->nullable();
+            $table->integer('deduction')->after('userID');
+            $table->integer('bonus')->after('userID');
             $table->dateTime('checkOut')->nullable()->after('date');
+            $table->integer('hour')->after('userID');
+            $table->integer('shiftID')->length(13)->unsigned()->after('userID');
+            $table->foreign('shiftID')
+            ->references('id')
+            ->on('shifts');
         });
     }
 
